8903a0dea87867258f5d7c4aff651fb70a969c21
aminphysicsandmetaphysics-glitch <amin.physicsandmetaphysics@gmail.com>
2025-08-31 10:18:42 -0700

    Add OIL alias and unit test

--- tests/test_normalize_symbol.py
+++ tests/test_normalize_symbol.py
@@ -3,3 +3,7 @@ from signal_bot import normalize_symbol
 
 def test_normalize_symbol_xau():
     assert normalize_symbol("XAU") == "XAUUSD"
+
+
+def test_normalize_symbol_oil():
+    assert normalize_symbol("OIL") == "USOIL"
